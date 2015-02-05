85ba12aa793112a3a02b43d04cf9d2b631a2b16f
Orrin Oliver <skormsghost@hotmail.com>
2015-02-05 16:12:25 -0600

    Added in a counter to tell you how many guesses you took.

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -7,7 +7,7 @@ int main(){
 int number = 42;
 int guess;
 bool loop = true;
-//int count = 0;
+int count = 0;
 
 cout << "What number am I thinking of?" << endl;
 cin >> guess;
@@ -15,18 +15,17 @@ cin >> guess;
 
 while(loop == true){
 	if(guess == number){
-	//count++;
-	//cout << "Nice job you guessed it correctly! It took " << count << " guesses to get the same number." << endl;
-	cout << "Nice job you guessed it correctly!" << endl;
+	count++;
+	cout << "Nice job you guessed it correctly! It took " << count << " guesses to get the same number." << endl;
 	loop = false;
 	}else if (guess < number){
 	cout << "Your number is less than the number I am thinking of." << endl;
-	//count++;
+	count++;
 	cin>>guess;
 	}else if (guess > number){
 	cout << "Your number is greater than the number I am thinking of." << endl;
 	cin>>guess;
-	//count++;
+	count++;
 	}
 }
 
