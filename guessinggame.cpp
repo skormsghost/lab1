fca678cc91075b652dcd4479094a5d4bc2880624
Orrin Oliver <skormsghost@hotmail.com>
2015-02-05 16:10:42 -0600

    Added in infinite guesses!

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -6,14 +6,28 @@ int main(){
 
 int number = 42;
 int guess;
+bool loop = true;
+//int count = 0;
 
 cout << "What number am I thinking of?" << endl;
 cin >> guess;
 
-if(guess == number){
-cout << "Nice job you guessed it correctly!" << endl;
-}else{
-cout << "Nice try but you lose!" << endl;
+
+while(loop == true){
+	if(guess == number){
+	//count++;
+	//cout << "Nice job you guessed it correctly! It took " << count << " guesses to get the same number." << endl;
+	cout << "Nice job you guessed it correctly!" << endl;
+	loop = false;
+	}else if (guess < number){
+	cout << "Your number is less than the number I am thinking of." << endl;
+	//count++;
+	cin>>guess;
+	}else if (guess > number){
+	cout << "Your number is greater than the number I am thinking of." << endl;
+	cin>>guess;
+	//count++;
+	}
 }
 
 }
